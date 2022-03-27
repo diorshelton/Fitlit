b2ffc8352942179ea54a3ff8a8dc5432b1b7c091
Dior.Shelton <dior.shelton@gmail.com>
2022-03-27 13:00:20 -0400

    Create test for UserHydration that returns oz of water user drank on specific day

--- src/UserHydration.js
+++ src/UserHydration.js
@@ -1,6 +1,15 @@
 class UserHydration {
-  constructor() {
-
+  constructor(data) {
+    this.data = data;
+  }
+  ozOnThisDay(date) {
+    let ounces = 0
+    this.data.filter( el => {
+      if (el.date === date) {
+        ounces += el.numOunces
+      }
+    });
+    return ounces;
   }
 }
 module.exports = UserHydration;
--- test/UserHydration-test.js
+++ test/UserHydration-test.js
@@ -1,16 +1,33 @@
 /* eslint-disable max-len */
 import { expect } from 'chai';
 import hydrationData from '../src/data/hydration';
+import HydrationRepository from '../src/HydrationRepository';
 import UserHydration from '../src/UserHydration'
 
 describe('UserHydration', () => {
   it('should be a function', () => {
-
     expect(UserHydration).to.be.a('function');
   })
 
   it('should have a data property', () => {
+    const hydrationRepo = new HydrationRepository(hydrationData);
 
-    expect
+    const user1 = hydrationRepo.findUserData(1);
+
+    const luisa = new UserHydration(user1);
+
+    expect(luisa.data).to.be.an('Array');
+  })
+
+  it('should return fluid ounces consumed on specified day', () => {
+    const hydrationRepo = new HydrationRepository(hydrationData);
+
+    const user1 = hydrationRepo.findUserData(1);
+
+    const luisa = new UserHydration(user1);
+    
+    const today = luisa.ozOnThisDay('2019/06/16');
+
+    expect(today).to.equal(69);
   })
 })
